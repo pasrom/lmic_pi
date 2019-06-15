ab93ade79fd3e03a527a3c93b9386cb65ced221a
roman <roman@roman-Parallels-Virtual-Platform>
2019-06-15 05:11:05 +0200

    Use EU-lora settings
    define CFG_eu868=1

--- lmic/config.h
+++ lmic/config.h
@@ -2,8 +2,8 @@
 #define _config_h_
 
 #define  CFG_sx1276_radio 1
-//#define CFG_eu868 1
-#define CFG_us915 1
+#define CFG_eu868 1
+//#define CFG_us915 1
 
 #define US_PER_OSTICK 50
 //#define  OSTICKS_PER_SEC 20000
